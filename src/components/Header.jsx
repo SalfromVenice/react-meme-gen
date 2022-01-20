432f2ffa8c6aeb6d296981fdce5bad017872ea00
Salvatore Strangio <sal.strangio@gmail.com>
2022-01-20 19:46:40 +0100

    Update

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <Header>
+      <img
+        src="http://www.pngall.com/wp-content/uploads/2015/05/Trollface.png"
+        alt="Problem?"
+      />
+      <p>Meme Generator</p>
+    </Header>
+  );
+};
+
+export default Header;
